dcf9c17b60a445a7702f64ecff367029ce4f129d
drearam51 <drearam51@gmail.com>
2025-11-05 13:38:53 -0500

    Ajuste CORS

--- server.js
+++ server.js
@@ -12,14 +12,14 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-// Configurar CORS para permitir el frontend local
-// app.use(
-//   cors({
-//     origin: "http://localhost:5173", // dominio del frontend Vite
-//     methods: ["GET", "POST", "PUT", "DELETE"],
-//     allowedHeaders: ["Content-Type", "Authorization"],
-//   })
-// );
+//Configurar CORS para permitir el frontend local
+app.use(
+  cors({
+    origin: "https://yuhu-sentiment.onrender.com", // dominio del frontend Vite
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+  })
+);
 
 
 // Conexión a la base de datos
